c887848128fb9b3bded38afd9eeeed10412ac911
JayashreeRavishankar <jayashree.sf3546@gmail.com>
2024-10-08 10:25:20 +0530

    Added sample

---
+++ MAUIPopup/MAUIPopup/Suggestion1.xaml.cs
@@ -0,0 +1,39 @@
+using Syncfusion.Maui.Popup;
+
+namespace MAUIPopup;
+
+public partial class Suggestion1 : ContentPage
+{
+    internal Label? PopupLabel { get; set; }
+    private SfPopup? Popup;
+    public Suggestion1()
+	{
+		InitializeComponent();
+        InitializePopup();
+	}
+    private void InitializePopup()
+    {
+        Popup = new SfPopup() { ShowFooter = true };
+        PopupLabel = new Label() { Text = "Custom Popup Content" };
+        Popup.ContentTemplate = new DataTemplate(() =>
+        {
+            return PopupLabel;
+        });
+        Popup.FooterTemplate = new DataTemplate(() =>
+        {
+            var button = new Button() { Text = "Change Text" };
+            button.Clicked += Button_Clicked;
+            return button;
+        });
+    }
+
+    private void Button_Clicked(object? sender, EventArgs e)
+    {
+        PopupLabel!.Text = "Text Changed";
+    }
+
+    private void Show_Popup(object sender, EventArgs e)
+    {
+        Popup!.Show();
+    }
+}
---
+++ MAUIPopup/MAUIPopup/Suggetion2.xaml.cs
@@ -0,0 +1,46 @@
+
+namespace MAUIPopup;
+
+public partial class Suggetion2 : ContentPage
+{
+    private Entry? entry;
+    private Entry? entry1;
+    public Suggetion2()
+	{
+		InitializeComponent();
+	}
+    private void ClickToShowPopup_Clicked(object sender, EventArgs e)
+    {
+        popup.Show();
+    }
+
+    private void StackLayout_ChildAdded(object sender, ElementEventArgs e)
+    {
+        if (e.Element is Entry)
+        {
+            var child = (Entry)e.Element;
+            if (child != null && child.StyleId == "Entry")
+            {
+                entry = child;
+            }
+            else if (child != null && child.StyleId == "Entry1")
+            {
+                entry1 = child;
+            }
+        }
+    }  
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        if (entry1!.Text == entry!.Text)
+        {
+            DisplayAlert("Status", "Login Successfully", "Ok");            
+        }
+        else
+        {
+            DisplayAlert("Status", "Login Failed", "Ok");
+        }
+        entry.Text = string.Empty;
+        entry1.Text = string.Empty;
+    }
+}
